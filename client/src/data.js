3e830fbbe9f4fb2e310ec9491f8323747397d1b0
JasonGitHubLiu <jason.liu002@umb.edu>
2023-04-08 03:12:23 -0400

    Made skills page responsive

---
+++ client/src/data.js
@@ -0,0 +1,60 @@
+let feArr = [
+  {
+    name: 'Node.JS',
+    img: 'https://res.cloudinary.com/ddl0mgnds/image/upload/v1680934860/Portfolio/icons8-nodejs-480_say6wq.png',
+  },
+  {
+    name: 'ReactJS',
+    img: 'https://res.cloudinary.com/ddl0mgnds/image/upload/v1680934860/Portfolio/icons8-react-native-480_znsjak.png',
+  },
+  {
+    name: 'JavaScript',
+    img: 'https://res.cloudinary.com/ddl0mgnds/image/upload/v1680934860/Portfolio/icons8-javascript-480_djtewi.png',
+  },
+  {
+    name: 'HTML',
+    img: 'https://res.cloudinary.com/ddl0mgnds/image/upload/v1680934860/Portfolio/icons8-html-5-480_cyn7az.png',
+  },
+  {
+    name: 'CSS',
+    img: 'https://res.cloudinary.com/ddl0mgnds/image/upload/v1680934861/Portfolio/icons8-css3-480_su8cy9.png',
+  },
+  {
+    name: 'Bootstrap',
+    img: 'https://res.cloudinary.com/ddl0mgnds/image/upload/v1680934318/Portfolio/icons8-bootstrap-480_ecm1mi.png',
+  },
+];
+
+let beArr = [
+  {
+    name: 'Node.JS',
+    img: 'https://res.cloudinary.com/ddl0mgnds/image/upload/v1680934860/Portfolio/icons8-nodejs-480_say6wq.png',
+  },
+  {
+    name: 'Mongo',
+    img: 'https://res.cloudinary.com/ddl0mgnds/image/upload/v1680934860/Portfolio/icons8-mongodb-480_hemc3l.png',
+  },
+  ,
+];
+
+let cmArr = [
+  {
+    name: 'Illustrator',
+    img: 'https://res.cloudinary.com/ddl0mgnds/image/upload/v1680937764/Portfolio/icons8-adobe-illustrator-480_ikuvle.png',
+  },
+  {
+    name: 'InDesign',
+    img: 'https://res.cloudinary.com/ddl0mgnds/image/upload/v1680937764/Portfolio/icons8-adobe-indesign-480_rsy1jq.png',
+  },
+  {
+    name: 'PhotoShop',
+    img: 'https://res.cloudinary.com/ddl0mgnds/image/upload/v1680937764/Portfolio/icons8-adobe-photoshop-480_xtxw1h.png',
+  },
+  {
+    name: 'PremierPro',
+    img: 'https://res.cloudinary.com/ddl0mgnds/image/upload/v1680937764/Portfolio/icons8-adobe-premiere-pro-480_qoxgmz.png',
+  },
+  ,
+];
+
+export default { beArr, feArr, cmArr };
--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,75 +1,53 @@
-// export default function About() {
-//   return (
-//     <>
-//       <h1>Hello About</h1>
-
-//       {/* <div class="container text-center">
-//   <div class="row">
-//     <div class="col">
-//     HTML
-//     <img className='skills' src={require('../images/html.png')}></img>
-//     </div>
-//     <div class="col">
-//     test
-//     <img className='skills' src={require('../images/html.png')}></img>
-//     </div>
-//     <div class="col">
-//       test2
-//     <img className='skills' src={require('../images/html.png')}></img>
-//     </div>
-//     <div class="col">
-//     <img className='skills' src={require('../images/html.png')}></img>
-//       test3
-//     </div>
-//   </div>
-// </div> */}
-//       <h1>SKILLS</h1>
-//       <h3>Frontend Development</h3>
-//       <section className="flex-container ">
-//         <div className=''>
-//           <img className='skills' src={require('../images/html.png')}></img>
-//            <strong><div>HTML</div></strong>
-//         </div>
-//         <div>
-//           <img className='skills' src={require('../images/css.png')}></img>
-//           <strong><div>CSS</div></strong>
-//         </div>
-//         <div>
-//           <img className='skills' src={require('../images/js.png')}></img>
-//           <strong><div>JavaScript</div></strong>
-//         </div>
-//         <div>
-//           <img className='skills' src={require('../images/bootstrap.png')}></img>
-//           <strong><div>Bootstrap</div></strong>
-//         </div>
-//         <div>
-//           <img className='skills' src={require('../images/react.png')}></img>
-//           <strong><div>React</div></strong>
-//         </div>
-//         <div>
-//           <img className='skills' src={require('../images/illustrator.png')}></img>
-//           <strong><div>Illustrator</div></strong>
-//         </div>
-//         {/* <div>
-//           <img className='skills' src={require('../images/indesign.png')}></img>
-//           <strong><div>InDesign</div></strong>
-//         </div> */}
-//         <div>
-//           <img className='skills' src={require('../images/photoshop.png')}></img>
-//           <strong><div>Photoshop</div></strong>
-//         </div>
-//         <div>
-//           <img className='skills' src={require('../images/premierpro.png')}></img>
-//           <strong><div>Premier Pro</div></strong>
-//         </div>
-//       </section>
-//     </>
-//   );
-// }
-
 import React from 'react';
+import data from '../data.js';
+
+const { feArr, beArr, cmArr } = data;
 
 export default function About() {
+  let fe = feArr.map((x) => {
+    return (
+      <div className="skillIcon d-flex flex-column text-center col-5 col-sm-3 col-md-3 ">
+        <div className="skillImg">
+          <img className="skills" src={`${x.img}`} alt={x.name} />
+        </div>
+
+        <div className='iconText'>
+          <strong>{x.name}</strong>
+        </div>
+      </div>
+    );
+  });
+  // end of feArr
+
+  let be = beArr.map((x) => {
+    return (
+      <div className="skillIcon d-flex flex-column text-center col-5 col-sm-3 col-md-3 ">
+        <div className="skillImg">
+          <img className="skills" src={`${x.img}`} alt={x.name} />
+        </div>
+
+        <div className='iconText'>
+          <strong>{x.name}</strong>
+        </div>
+      </div>
+    );
+  });
+  // end of beArr
+
+  let cm = cmArr.map((x) => {
+    return (
+      <div className="skillIcon d-flex flex-column text-center col-5 col-sm-3 col-md-3 ">
+        <div className="skillImg">
+          <img className="skills" src={`${x.img}`} alt={x.name} />
+        </div>
+
+        <div className='iconText'>
+          <strong>{x.name}</strong>
+        </div>
+      </div>
+    );
+  });
+  // end of cmArr
   return (
     <>
       <h1>Hello About</h1>
@@ -77,137 +55,31 @@ export default function About() {
 
       <br />
       <br />
-      <h4>Frontend Development</h4>
-      <section className="flex-container">
-        <div className="skill">
-          <img
-            className="skills"
-            src={require('../images/node.png')}
-            alt="Node"
-          />
-          <strong>
-            <div>Node.JS</div>
-          </strong>
-        </div>
-        <div className="skill">
-          <img
-            className="skills"
-            src={require('../images/react.png')}
-            alt="React"
-          />
-          <strong>
-            <div>ReactJS</div>
-          </strong>
-        </div>
-        <div className="skill">
-          <img
-            className="skills"
-            src={require('../images/js.png')}
-            alt="JavaScript"
-          />
-          <strong>
-            <div>JavaScript</div>
-          </strong>
-        </div>
-        <div className="skill">
-          <img
-            className="skills"
-            src={require('../images/html.png')}
-            alt="HTML"
-          />
-          <strong>
-            <div>HTML</div>
-          </strong>
-        </div>
-        <div className="skill">
-          <img
-            className="skills"
-            src={require('../images/css.png')}
-            alt="CSS"
-          />
-          <strong>
-            <div>CSS</div>
-          </strong>
-        </div>
-        <div className="skill">
-          <img
-            className="skills"
-            src={require('../images/bootstrap.png')}
-            alt="Bootstrap"
-          />
-          <strong>
-            <div>Bootstrap</div>
-          </strong>
-        </div>
-      </section>{/* End of Frontend */} 
-      <br/><br/>
-      
-      <h4>Backend Development</h4>
-      <section className="flex-container">
-        <div className="skill">
-          <img
-            className="skills"
-            src={require('../images/node.png')}
-            alt="Node"
-          />
-          <strong>
-            <div>Node.JS</div>
-          </strong>
-        </div>
-        <div className="skill">
-          <img
-            className="skills"
-            src={require('../images/mongo.png')}
-            alt="Mongo"
-          />
-          <strong>
-            <div>MongoDB</div>
-          </strong>
-        </div>
+      <strong>
+        <h1>Frontend Development</h1>
+      </strong>
 
-      </section>{/* end of Backend */}
-      
+      <section className="flex-container container-fluid">{fe}</section>
+
       <br />
       <br />
-      <h4>Creative Media</h4>
-      <section className="flex-container">
-        <div className="skill">
-          <img
-            className="skills"
-            src={require('../images/illustrator.png')}
-            alt="Illustrator"
-          />
-          <strong>
-            <div>Illustrator</div>
-          </strong>
-        </div>
-        <div>
-          <img className="skills" src={require('../images/indesign.png')}></img>
-          <strong>
-            <div>InDesign</div>
-          </strong>
-        </div>
-        <div className="skill">
-          <img
-            className="skills"
-            src={require('../images/photoshop.png')}
-            alt="Photoshop"
-          />
-          <strong>
-            <div>Photoshop</div>
-          </strong>
-        </div>
-        <div className="skill">
-          <img
-            className="skills"
-            src={require('../images/premierpro.png')}
-            alt="Premier Pro"
-          />
-          <strong>
-            <div>Premier Pro</div>
-          </strong>
-        </div>
-      </section>
+      <br />
+      <br />
+
+      <h1>Backend Development</h1>
+      <section className="flex-container container-fluid">{be}</section>
+      {/* end of Backend */}
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <h1>Creative Media</h1>
+      <section className="flex-container container-fluid">{cm}</section>
+      <br />
+      <br />
+      <br />
+      <br />
     </>
   );
 }
