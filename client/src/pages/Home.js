82a12278f7f0751b65f48fbd9379b49fe1e66b87
JasonGitHubLiu <jason.liu002@umb.edu>
2023-04-09 05:58:23 -0400

    Added Resume to home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,11 +7,21 @@ export default function Home() {
         I'm a full stack web developer with a passion for creating and learning
         new things.
       </h3>
+      {/* <div className='bg'>
+        <img
+          src={require('../images/endless-constellation.png')}
+          alt="bg"
+        ></img>
+      </div> */}
       <br />
       <br />
       <div className="btn-group" role="group">
         <Link to="https://www.linkedin.com/in/jasonliu-/" target="_blank">
-          <button type="button" className="btn btn-secondary" style={{ width: '150px', marginRight: '10px' }}>
+          <button
+            type="button"
+            className="btn btn-secondary"
+            style={{ width: '150px', marginRight: '10px' }}
+          >
             <img
               src={
                 'https://res.cloudinary.com/ddl0mgnds/image/upload/v1681009128/Portfolio/icons8-linkedin-48_b6becc.png'
@@ -22,7 +32,11 @@ export default function Home() {
           </button>
         </Link>
         <Link to="https://github.com/JasonGitHubLiu" target="_blank">
-          <button type="button" className="btn btn-secondary" style={{ width: '150px', marginLeft: '10px' }}>
+          <button
+            type="button"
+            className="btn btn-secondary"
+            style={{ width: '150px', marginLeft: '10px', marginRight: '10px' }}
+          >
             <img
               src={
                 'https://res.cloudinary.com/ddl0mgnds/image/upload/v1681008367/Portfolio/icons8-github-48_mh4i7k.png'
@@ -33,7 +47,25 @@ export default function Home() {
           </button>
         </Link>
       </div>
+        <button
+          type="button"
+          className="btn btn-secondary"
+          style={{ width: '150px', marginLeft: '10px' }}
+          onClick={() =>
+            window.open(
+              'https://drive.google.com/file/d/1sWi2yz1CBy1Sl0V216A3PV9hC5_WWHiC/view?usp=sharing',
+              '_blank'
+            )
+          }
+        >
+          <img
+            src={
+              'https://res.cloudinary.com/ddl0mgnds/image/upload/v1681033283/Portfolio/icons8-resume-48_vcjlyl.png'
+            }
+            alt="Resume"
+          ></img>
+          Resume
+        </button>
     </>
   );
 }
-
