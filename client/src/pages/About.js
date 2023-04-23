303db5024efa17d04a14207539e1404a3a9b2652
JasonGitHubLiu <jason.liu002@umb.edu>
2023-04-23 04:42:50 -0400

    Updated about

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -23,13 +23,13 @@ export default function About() {
       <h1> About</h1>
       <br></br>
       <div className='aboutDiv'>
-      <h3>I am from Boston, Massachusetts and my name is Jason Liu.</h3>
+      <h3>I am from Boston, Massachusetts, and my name is Jason Liu.</h3>
       <br></br>
-      <h3>My academic pursuits at UMass Boston were focused on Entrepreneurship and Marketing, which led to the acquisition of my Bachelor's degree in Management.</h3>
+      <h3>My academic pursuits at UMass Boston focused on Entrepreneurship and Marketing, which led to my acquiring my Bachelor's in Management.</h3>
       <br></br>
-      <h3>After graduation, I decided to shift gears towards the technology industry and thus, enrolled in the Software Engineering Bootcamp provided by Per Scholas, which I successfully completed.</h3>
+      <h3>After graduation, I decided to shift gears toward the technology industry and, thus, enrolled in the Software Engineering Bootcamp provided by Per Scholas, which I completed.</h3>
       <br></br>
-      <h3>I am a full stack MERN developer</h3>
+      <h3>I am a full-stack MERN developer.</h3>
       </div>
       <br></br>
       <section className="flex-container container-fluid">{ab}</section>
